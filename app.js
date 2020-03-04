5a83b6b8add1cb0aef3dc2172c5ab585558bebf9
Niogy, Chandrim <chandrim.niogy@accenture.com>
2020-03-04 15:21:30 +0530

    check branch

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/save', authority.create);
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
     error.status= 404;
-    next(error);
+    next(error)
 })
 
 app.use((error,req,res,next)=>{
